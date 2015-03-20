19131f0049b0597458d77c160a66a4966caa7a58
pylearner0 <pylearner@outlook.com>
2015-03-20 01:09:13 -0400

    Small edit in the comments

--- Trolling Program.py
+++ Trolling Program.py
@@ -1,7 +1,8 @@
-#@uthor: Pylearner0
+#@uthor: Pylearner0 AKA CaptainOffull
 #Python version: 3.1.1
 #A program that I created as a beginnner that you can use to troll your friends
 #I will add more things as I learn Python
+#I'm currently learning from the book "Python Programming for the Absolute Beginner 3rd Edition")
 
 input("Press enter to see something cool\n")
 
